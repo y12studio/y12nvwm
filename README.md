07956410d0665fd3360ae810715e52ed2523a36f
y12studio <y12studio@gmail.com>
2015-05-07 15:17:18 +0800

    alias.sh updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Install on DigitalOcean/DreamCompute
+Install on DigitalOcean/Ubuntu 14.04 x64
 -----------
 ```
 # adduser xuser
@@ -10,14 +10,15 @@ $ git clone https://github.com/y12studio/y12nvwm.git
 $ cd y12nvwm && chmod +x install.sh && ./install.sh
 $ sudo reboot now
 ```
-
 wordpress conf
 -----
 ```
 # su - xuser
 $ cd y12nvwm
 $ source alice.sh
+$ sed -i "s|192.168.2.73|blog.y12.tw|g" docker-compose.yml
 $ dcup
-$ wp2sh
-# /wpinstall.sh
+$ dc ps
+$ dc logs
+$ wp2sh /wpinstall.sh
 ```
--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 DCP=nvwm
 alias dc='docker-compose -p ${DCP}'
-alias wp2sh='docker exec -i -t ${DCP}_wordpress_1 bash'
-alias varnish2sh='docker exec -i -t ${DCP}_varnish_1 bash'
-alias nginx2sh='docker exec -i -t ${DCP}_nginx_1 bash'
-alias dcup='dc stop && dc rm && dc build && dc up -d && dc logs'
+alias wp2sh='docker exec -i -t ${DCP}_wordpress_1'
+alias varnish2sh='docker exec -i -t ${DCP}_varnish_1'
+alias nginx2sh='docker exec -i -t ${DCP}_nginx_1'
+alias dcup='dc stop && dc rm && dc build && dc up -d'
